170c538a9309faf75f6240cd402cff7017a2d83f
Stefan Kofler <kofler.s90@gmail.com>
2021-04-26 11:22:15 +0100

    feat: click adds X or O to square

--- src/index.js
+++ src/index.js
@@ -2,33 +2,48 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null
-    };
-  }
-
-  render() {
-    return (
-      <button
-        className="square"
-        onClick={() => this.setState({ value: 'X'})}
-      >
-        {this.state.value}
-      </button>
-    );
-  }
+function Square(props) {
+  return (
+    <button
+      className="square"
+      onClick={props.onClick}
+    >
+      {props.value}
+    </button>
+  );
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+      xIsNext: true,
+    }
+  }
+
+  getPlayerString(xIsNext) {
+    return xIsNext ? 'X': 'O';
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.getPlayerString(this.state.xIsNext);
+    const xIsNext = !this.state.xIsNext;
+    this.setState({ xIsNext, squares });
+  }
+
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)} 
+      />
+    );
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = `Next player: ${this.getPlayerString(this.state.xIsNext)}`;
 
     return (
       <div>
